f8cab7c99507e6458c18760d79b648374dc67494
Ethan Jacinto <ethankylejacinto@gmail.com>
2024-04-05 22:26:27 -0700

    feat: additional game info in detailed view

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -15,16 +15,28 @@ function GamePage({ toggle, data }) {
       }
     transition-all`}
     >
-      <div className="flex flex-col justify-center items-center mt-[10rem] ml-[15rem] w-[75rem] h-[50rem] bg-[#131313] rounded-3xl">
+      <div className="flex flex-col justify-center items-center mt-[10rem] ml-[15rem] w-[85rem] h-[55rem] bg-[#131313] rounded-3xl">
         <div className="flex flex-col justify-center items-center gap-2">
           <h1 className=" text-6xl font-bold">{name}</h1>
           <h1 className="text-4xl">Rating: {game.rating} / 5</h1>
+          <h1 className="text-4xl">
+            Total Reviewer Playtime: {game.playtime} hours
+          </h1>
           <h1 className="text-4xl">Release Date: {game.released}</h1>
           <h1 className="text-4xl">Owned: {game.added_by_status.owned}</h1>
           <h1 className="text-4xl">
             Metacritic Score: {game.metacritic} / 100
           </h1>
-          <h1 className="text-4xl">Owned: {game.esrb_rating.name}</h1>
+          <h1 className="text-4xl">ESRB Rating: {game.esrb_rating.name}</h1>
+          <div className="flex  gap-4">
+            <h1 className="text-4xl">Platforms: </h1>
+            {game.parent_platforms.map((platform, index) => (
+              <h1 className="text-4xl underline" key={index}>
+                {platform.platform.name}
+              </h1>
+            ))}
+          </div>
+
           <img
             src={game.background_image}
             width={500}
